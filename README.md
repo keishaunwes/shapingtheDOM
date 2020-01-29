3e9a23e26731c15a83b1b613411102b4c61f5bbc
keishaun wesley <keishaunwesley@gmail.com>
2020-01-28 22:43:59 -0600

    you ever mess up your code and kind of have to start over

--- README.md
+++ README.md
@@ -13,8 +13,6 @@ Number each example in your html page to correspond with the objective numbers b
 2. Make a button and text box (you can just put them in your HTML). When the button is clicked, display an alert with the message that is typed in the text box.
 3. Create a `div` in HTML. Without using CSS :hover, make the div change to a different background color when your mouse hovers over it. The div should return to its original color when the mouse exits the div.
 4. Put some text in a paragraph. Make it where when you click on the paragraph, the color of the text switches to red. Once you get that working, try to rewrite your code to make it switch to a random color each click (you don't have to show the code for just red once you get random working).
+
 5. Add a button and an empty div. When the button is clicked, add a `span` that contains your name to the empty div.
-
-
-
 6. Create a button and a `ul` in your HTML. In JavaScript, create an array containing the names of your friends (at least 10. If you don't have that many, include your imaginary ones). When the button is clicked, add each friend's name as an `li` to the `ul` on the page.
--- master.js
+++ master.js
@@ -1,21 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
   let button = document.createElement("button");
-  let btnText = document.createTextNode("click me");
+  let btnText = document.createTextNode("click me for JS");
   button.appendChild(btnText);
   document.body.append(button);
-  let textBox = document.getElementById("textBox");
   div = document.createElement("div");
   document.body.append(div);
   div.style.height = "100px";
   div.style.width = "100px";
-  let rgbRED = Math.floor(Math.random() * 4);
-  let rgbB = Math.floor(Math.random() * 40);
-  let rgnGR = Math.floor(Math.random() * 400);
+
   let div2 = document.createElement("div");
-  let nameText = document.createTextNode("keishaun");
-  //    button.appendChild(btnText);
+  let nameText = document.createTextNode("Keishaun");
+ 
   document.body.append(div2);
   let friends = [
+    "",
     "Marcus",
     "Genesis",
     "Coby",
@@ -27,9 +25,8 @@ document.addEventListener("DOMContentLoaded", function() {
     "JONJON",
     "PJ"
   ];
-  let counter = 0;
   let button2 = document.createElement("button");
-  let btnText2 = document.createTextNode("click to learn more");
+  let btnText2 = document.createTextNode("click to learn my name");
   button2.appendChild(btnText2);
   document.body.append(button2);
 
@@ -40,11 +37,13 @@ document.addEventListener("DOMContentLoaded", function() {
   span.appendChild(paragr);
   div2.appendChild(span);
 
+  let divdom = document.createElement("div");
+  document.body.append(divdom);
   let button3 = document.createElement("button");
-  let btnText3 = document.createTextNode("All my dear friends");
+  let btnText3 = document.createTextNode("Friends");
   button3.appendChild(btnText3);
-  document.body.append(button3);
-
+  divdom.appendChild(button3);
+  randomFriend = [Math.floor(Math.random() * friends.length)];
   let colors = [
     " aqua",
     "black",
@@ -90,19 +89,102 @@ document.addEventListener("DOMContentLoaded", function() {
     span.appendChild(name);
   });
 
-  button3.addEventListener("click", function() {
-      
-      if (counter <= friends.length-1) {
-        let ul = document.createElement("ul");
-        document.body.append(ul);
-        let li = document.createElement("li");
-        ul.appendChild(li);
-      li.innerHTML = friends[counter];
-    } else{
-alert("thats it buddy")
+  let counter = 0;
 
+  button3.addEventListener("click", function() {
+    counter++;
+
+    if (counter <= friends.length - 1) {
+      let ull = document.createElement("ul");
+      divdom.appendChild(ull);
+      let lil = document.createElement("li");
+      ull.appendChild(lil);
+      lil.innerHTML = friends[counter];
+    } else {
+      alert("thats it buddy");
     }
 
-    counter++;
+    console.log(counter);
+  });
+});
+
+$(document).ready(function() {
+  let buttonj = $("<button>click me for jquery</button>");
+  let divj = $("<div></div>");
+  $("body").append(buttonj);
+  let inputj = $("<input type ='text'>");
+  let colorsj = [
+    " aqua",
+    "black",
+    "blue",
+    "fuchsia",
+    "gray",
+    "grey",
+    "green",
+    "lime",
+    "maroon",
+    "navy",
+    "olive",
+    "purple",
+    " red",
+    "silver",
+    "teal",
+    "yellow"
+  ];
+  divj.css({ height: "100px", width: "100px" });
+  para = $("<p>Hello Jquery</p>");
+  let divfriends = $("<div></div>");
+  let btnfriends = $("<button>friends</button>");
+
+  $("body").append(inputj);
+  $("body").append(divj);
+  $("body").append(para);
+  $("body").append(divfriends);
+  $("body").append(btnfriends);
+
+  buttonj.click(function() {
+    alert(inputj.val());
+  });
+  divj.mouseover(function() {
+    divj.css("background-color", "aqua");
+  });
+  divj.mouseout(function() {
+    divj.css("background-color", "white");
+  });
+
+  para.click(function() {
+    let randomColor = colorsj[Math.floor(Math.random() * colorsj.length)];
+    para.css("color", randomColor);
+  });
+
+  let counterj = 0;
+  let divquery = $("<div></div>");
+  btnfriends.click(function() {
+    let friendsj = [
+      "",
+      "Marcus",
+      "Genesis",
+      "Coby",
+      "Jon",
+      "Keyontay",
+      "Kallie",
+      "Chase",
+      "Jeremey",
+      "JONJON",
+      "PJ"
+    ];
+
+    if (counterj <= friendsj.length - 2) {
+      counterj++;
+
+      let ul = $("<ul> </ul>");
+      divquery.append(ul);
+      $("body").append(divquery);
+      let li = $("<li></li>");
+      ul.append(li);
+      li.text(friendsj[counterj]);
+    } else {
+      alert("thats it buddy");
+    }
   });
 });
